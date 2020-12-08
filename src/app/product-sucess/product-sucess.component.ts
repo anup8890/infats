9d19fd7e53abb93ab5c7964d0e63ddd4edccc55c
anup8890 <anup8890@gmail.com>
2020-12-08 15:07:03 +0530

    Add files via upload

---
+++ src/app/product-sucess/product-sucess.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-product-sucess',
+    templateUrl:'product-sucess.component.html',
+    styleUrls:['product-sucess.component.scss']
+})
+export class ProductSucessComponent{
+    ApplicationName="jer";
+    ApplicationStatus="Active";
+    ApplicationGroupName="GRN";
+    Description="";
+    Comment="OK";
+}
