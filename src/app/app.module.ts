08d2955fd637487e77953536970bec5399e4fe1e
anup8890 <anup8890@gmail.com>
2020-12-08 18:31:44 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { ProductSucessComponent } from './product-sucess/product-sucess.component';
-import { ProductRegistrationComponent } from './productRegistration/productRegistration.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductSucessComponent } from 'src/app/product-sucess/product-sucess.component';
+import { ProductRegistrationComponent } from 'src/app/productRegistration/productRegistration.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonService } from './_service/common.service';
 import { ProductService } from './_service/product.service';
 
@@ -17,15 +17,16 @@ import { ProductService } from './_service/product.service';
     AppComponent,
     ProductSucessComponent,
     ProductRegistrationComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
-      {path: 'product-sucess', component:ProductSucessComponent},
       {path: 'productRegistration', component:ProductRegistrationComponent},
+      {path: 'product-sucess', component:ProductSucessComponent},
       {path: '', redirectTo: '/productRegistration', pathMatch: 'full'},
       {path: '**', component:PageNotFoundComponent}
     ]),
