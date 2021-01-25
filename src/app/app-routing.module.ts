23cbded92891ccf3a985acf074539862ae9a12a8
anup8890 <anup8890@gmail.com>
2021-01-25 16:50:59 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductSucessComponent } from 'src/app/product-sucess/product-sucess.component';
+import { ProductRegistrationComponent } from 'src/app/productRegistration/productRegistration.component';
+import { PageNotFoundComponent } from 'src/app/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'productRegistration', component:ProductRegistrationComponent},
+      {path: 'product-sucess', component:ProductSucessComponent},
+      {path: '',component:AppComponent},
+      {path: '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
